370be7712cb5bfe121ca8ceb43bd0a371d3c6f5f
stswoon <stswoon@yandex.ru>
2015-07-26 17:20:58 +0300

    first commit

---
+++ ideaFeatures/src/nodomain/stswoon/text/Editing.java
@@ -0,0 +1,16 @@
+package nodomain.stswoon.text;
+
+public class Editing {
+    /**
+     * Test square selection - press Alt+Shift+Insert and change 1 2 3 -> 2 3 4
+     * text 1 2 3 zzz
+     * text 1 2 3 www
+     * text 1 2 3 qqq
+     */
+    private void multipleCursors() {
+        int z = 0;
+        //press Alt+Shift+mouse click and change z -> 2*z
+        int x = z + 1;
+        int y = x + z + 1;
+    }
+}
