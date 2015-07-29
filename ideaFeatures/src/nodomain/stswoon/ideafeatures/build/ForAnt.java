a600b6d820b5b2662e4ed426db8c361a85aa4fe7
stswoon <stswoon@yandex.ru>
2015-07-29 22:43:00 +0300

    try

---
+++ ideaFeatures/src/nodomain/stswoon/ideafeatures/build/ForAnt.java
@@ -0,0 +1,8 @@
+package nodomain.stswoon.ideafeatures.build;
+
+public class ForAnt {
+    public static void main(String[] args) {
+        //https://ant.apache.org/manual/tutorial-HelloWorldWithAnt.html#prepare
+        System.out.println("Hello World by Ant");
+    }
+}
---
+++ ideaFeatures/src/nodomain/stswoon/ideafeatures/build/ForMaven.java
@@ -0,0 +1,7 @@
+package nodomain.stswoon.ideafeatures.build;
+
+public class ForMaven {
+    public static void main(String[] args) {
+        System.out.println("Hello World by Maven");
+    }
+}
