cbf80a28afa72f21378d97068200f6a137d92421
nekhozhin <stswoon@yandex.ru>
2015-09-02 08:18:28 +0400

    try

--- patterns/src/nodomain/stswoon/patterns/headfirst/pizzastrore/MainPizza.java
+++ patterns/src/nodomain/stswoon/patterns/headfirst/pizzastrore/MainPizza.java
@@ -1,13 +1,18 @@
 package nodomain.stswoon.patterns.headfirst.pizzastrore;
 
-import nodomain.stswoon.patterns.headfirst.pizzastrore.SimplePizzaFactory;
 import nodomain.stswoon.patterns.headfirst.pizzastrore.pizza.Pizza;
+import nodomain.stswoon.patterns.headfirst.pizzastrore.store.ChicagoPizzaStore;
+import nodomain.stswoon.patterns.headfirst.pizzastrore.store.NYPizzaStore;
 import nodomain.stswoon.patterns.headfirst.pizzastrore.store.PizzaStore;
 
 public class MainPizza {
     public static void main(String[] args) {
-        PizzaStore pizzaStore = new PizzaStore(new SimplePizzaFactory());
+        PizzaStore pizzaStore = new NYPizzaStore();
         Pizza pizza = pizzaStore.orderPizza("clam");
+        System.out.println(pizza.getClass().getSimpleName());
+
+        pizzaStore = new ChicagoPizzaStore();
+        pizza = pizzaStore.orderPizza("clam");
         System.out.println(pizza.getClass().getSimpleName());
     }
 }
