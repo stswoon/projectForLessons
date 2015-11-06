f08b78132d604f95caa5c568472a8e45f9b6db3f
nekhozhin <stswoon@yandex.ru>
2015-11-06 09:06:59 +0400

    try

--- other2/src/main/java/javascript/bindCallApply.js
+++ other2/src/main/java/javascript/bindCallApply.js
@@ -5,12 +5,12 @@ var context = {
 function returnFoo() {
     return this.foo;
 }
-console.log(returnFoo());
+//console.log(returnFoo());
 
 var bound = returnFoo.bind(context);
-console.log(bound());
-console.log(returnFoo.call(context));
-console.log(returnFoo.apply(context));
+//console.log(bound());
+//console.log(returnFoo.call(context));
+//console.log(returnFoo.apply(context));
 
 context.returnFoo = returnFoo;
-console.log(context.returnFoo());
+//console.log(context.returnFoo());
---
+++ other2/src/main/java/javascript/objects.js
@@ -0,0 +1,52 @@
+var getSetObject = {
+    test1: [1, 2, 3],
+    get height() {
+        return this.test1.length;
+    },
+    get test1() {
+        return [5, 6, 7];
+    },
+
+    test2: 'qqq',
+    get test2() {
+        return this.test2;
+    },
+
+    _test4: '111',
+    get test4() {
+        return this._test4;
+    }
+
+};
+console.log(getSetObject);
+Object.defineProperty(getSetObject,
+    "_test3", {
+        get: function () {
+            return this.test3;
+        },
+        set: function (value) {
+            this.test3 = value;
+        }
+    }
+);
+
+console.log(getSetObject.height);
+console.log(getSetObject.test1);
+getSetObject.test1 = [0, 8];
+console.log(getSetObject.test1);
+
+//console.log(getSetObject.test2);//endless recursion
+
+console.log(getSetObject.test3);
+getSetObject.test3 = 'www';
+console.log(getSetObject.test3);
+console.log(getSetObject._test3);
+getSetObject._test3 = 'bbb';
+console.log(getSetObject._test3);
+console.log(getSetObject.test3);
+
+console.log(getSetObject.test4);
+getSetObject.test4 = '222'; //property is redonly because there is no set methid
+console.log(getSetObject.test4);
+
+
