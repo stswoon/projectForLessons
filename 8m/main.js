45ad5c9356a0d723682b12c045aaa9a5a8101b87
Anatoly Nekhozhin <Nekhozhin@NetCracker.com>
2017-02-22 09:53:05 +0400

    8m

--- 8m/main.js
+++ 8m/main.js
@@ -7,6 +7,13 @@ function nextPage() {
 	document.getElementById("page"+pageIndex).classList.remove("active")
 	pageIndex++;
 	document.getElementById("page"+pageIndex).classList.add("active")
+
+	if (pageIndex == 11) {
+		result();
+	} else if (pageIndex == 12) {
+		salut();
+	}
+
 }
 
 function answer(taskNumber, answer) {
