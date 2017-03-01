7228050d30c2a7252ab3efb276922dbba64779ef
stswoon <stswoon@yandex.ru>
2017-03-01 12:24:59 +0400

    8m

--- 8m/main.js
+++ 8m/main.js
@@ -1,23 +1,23 @@
 var pageIndex = 0;
-var RIGHT_ASWERS = ['7 years', 'sun', 'blue', 'green'];
+var RIGHT_ASWERS = ['7 years', 'sun', 'blue', 'green', 'son', 'tola', 'family', 'crazy'];
 var answers = [];
 
 
 function nextPage() {
-	document.getElementById("page"+pageIndex).classList.remove("active")
+	document.getElementById("page"+pageIndex).classList.remove("active");
 	pageIndex++;
-	document.getElementById("page"+pageIndex).classList.add("active")
+	document.getElementById("page"+pageIndex).classList.add("active");
 
-	if (pageIndex == 11) {
+	if (pageIndex == 9) {
 		result();
-	} else if (pageIndex == 12) {
+	} else if (pageIndex == 10) {
 		salut();
 	}
 
 }
 
 function answer(taskNumber, answer) {
-	answers.push[answer]
+	answers.push[answer];
 	nextPage();
 }
 
@@ -26,5 +26,6 @@ setTimeout(function() {
 	nextPage();
 	nextPage();
 	nextPage();
-	//nextPage();
+	nextPage();
+	nextPage();
 }, 500);
