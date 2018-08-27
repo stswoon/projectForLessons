9cff9d48911a66c4424f6d3f6c3727bac5865ab4
Nekhozhin <Nekhozhin@NetCracker.com>
2018-08-27 08:55:50 +0400

    css

--- css/main.js
+++ css/main.js
@@ -11,3 +11,25 @@ $(function () {
         $(".post").show(500);
     });
 });
+
+$(document).ready(function () {
+    //https://owlcarousel2.github.io/OwlCarousel2/demos/basic.html
+    $(".owl-carousel").owlCarousel({
+        //loop: false,
+        //pagination: true,
+        margin: 10,
+        //nav: true,
+        dots: true,
+        responsive: {
+            0: {
+                items: 2
+            },
+            600: {
+                items: 5
+            },
+            1000: {
+                items: 8
+            }
+        }
+    });
+});
