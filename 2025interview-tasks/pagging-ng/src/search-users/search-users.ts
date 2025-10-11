b0f04b56f8167b157d3e8608f2fdf804f6df825d
stswoon <xx@xx.xx>
2025-10-11 23:36:09 +0400

    ng pagging

---
+++ 2025interview-tasks/pagging-ng/src/search-users/search-users.ts
@@ -0,0 +1,118 @@
+import {Component, inject, OnDestroy, OnInit} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {HttpClient} from '@angular/common/http';
+import {map, Subscription} from 'rxjs';
+
+//todo: paging, debounce, service, abort
+
+interface User {
+  id: string;
+  firstName: string;
+  lastName: string;
+}
+
+@Component({
+  selector: 'search-users',
+  templateUrl: './search-users.html',
+  imports: [
+    FormsModule,
+    // AsyncPipe
+  ],
+})
+export class SearchUsers implements OnInit, OnDestroy {
+  private http = inject(HttpClient);
+  private requestSubscription?: Subscription;
+
+  query = '';
+  loading: boolean = false;
+  hasError: boolean = false;
+  //$users?: Observable<User[]>;
+  users?: User[];
+
+  totalPages: number = 0;
+  page: number = 1;
+
+  ngOnInit(): void {
+    this.doSearch();
+  }
+
+  debouncedFn = debounce(() => {
+    this.doSearch();
+  });
+
+  handleQueryChange() {
+    console.log(`change query = |${this.query}|`);
+    this.requestSubscription?.unsubscribe();
+    this.debouncedFn();
+  }
+
+  private doSearch() {
+    console.log(`Run search query = |${this.query}|`);
+    this.loading = true;
+    this.hasError = false;
+    const url = getUrl(this.query, this.page);
+    const $httpRequest = this.http.get(url);
+    this.requestSubscription = $httpRequest.pipe(
+      map((rawResponse: any) => {
+        return convertToUsers(rawResponse);
+      }),
+      // catchError((err) => {
+      //   console.error("Failed to load: ", err)
+      //   this.hasError = !!err;
+      //   return of([]);
+      // }),
+      // finalize(() => {
+      //   this.loading = false;
+      // })
+    ).subscribe(({
+      next: (res => {
+        this.totalPages = res.totalPages;
+        this.users = res.users;
+        this.loading = false;
+      }),
+      error: (err => {
+        console.error("Failed to load: ", err)
+        this.hasError = !!err;
+        this.loading = false;
+        this.users = [];
+      })
+    }));
+  }
+
+  ngOnDestroy(): void {
+    this.requestSubscription?.unsubscribe()
+  }
+
+  previous() {
+    this.page--;
+    this.doSearch();
+  }
+
+  next() {
+    this.page++;
+    this.doSearch();
+  }
+}
+
+const LIMIT = 10;
+const DELAY = 2000;
+const getUrl = (query: string, page: number): string => {
+  const skip = (page - 1) * LIMIT;
+  return `https://dummyjson.com/users/search?q=${query}&limit=${LIMIT}&skip=${skip}&delay=${DELAY}`;
+}
+
+function convertToUsers(rawData: any): { users: User[], totalPages: number } {
+  const users = (rawData.users as User[]).map(({id, firstName, lastName}) => ({id, firstName, lastName}));
+  const totalPages = Math.ceil(rawData.total / LIMIT);
+  return {users, totalPages}
+}
+
+function debounce<T extends (...args: any[]) => void>(fn: T, delay: number = 500): (...args: Parameters<T>) => void {
+  let timerId: ReturnType<typeof setTimeout>;
+  return function (this: ThisParameterType<T>, ...args: Parameters<T>) {
+    clearTimeout(timerId);
+    timerId = setTimeout(() => {
+      fn.apply(this, args)
+    }, delay);
+  };
+}
---
+++ 2025interview-tasks/pagging-ng/src/serach-users-2/search-users-2.ts
@@ -0,0 +1,137 @@
+import {Component, inject, Injectable, OnDestroy, OnInit} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {HttpClient} from '@angular/common/http';
+import {BehaviorSubject, catchError, map, Observable, of, Subscription, switchMap} from 'rxjs';
+
+//todo: service, signal in ngInit
+
+interface User {
+  id: string;
+  firstName: string;
+  lastName: string;
+}
+
+type SearchUsersServiceResponse = {
+  loading: boolean;
+  data?: {
+    users: User[];
+    totalPages: number
+  }
+  error?: any;
+}
+
+@Injectable({providedIn: 'root'})
+class SearchUsersService {
+  private http = inject(HttpClient);
+
+  getUsers(query: string, page: number): Observable<SearchUsersServiceResponse> {
+    const url = getUrl(query, page);
+    // const $httpRequest = this.http.get(url);
+    const $response = new BehaviorSubject<SearchUsersServiceResponse>({loading: true})
+    return $response.pipe(
+      switchMap((_) => this.http.get(url)),
+      map((rawResponse: any) => convertToUsers(rawResponse)),
+      map(data => ({loading: false, data})),
+      catchError((err) => {
+        console.error("Failed to load: ", err)
+        return of({loading: false, error: err});
+      })
+    )
+  }
+}
+
+@Component({
+  selector: 'search-users-2',
+  templateUrl: './search-users-2.html',
+  imports: [
+    FormsModule,
+    // AsyncPipe
+  ],
+})
+export class SearchUsers2 implements OnInit, OnDestroy {
+  private searchUsersService = inject(SearchUsersService);
+  private requestSubscription?: Subscription;
+
+  query = '';
+  loading: boolean = false;
+  hasError: boolean = false;
+  //$users?: Observable<User[]>;
+  users?: User[];
+
+  totalPages: number = 0;
+  page: number = 1;
+
+  ngOnInit(): void {
+    this.doSearch();
+  }
+
+  debouncedFn = debounce(() => {
+    this.doSearch();
+  });
+
+  //todo: if new query make page = 0
+
+  handleQueryChange() {
+    console.log(`change query = |${this.query}|`);
+    this.requestSubscription?.unsubscribe();
+    this.debouncedFn();
+  }
+
+  private doSearch() {
+    console.log(`Run search query = |${this.query}|, ${this.page}`);
+    this.loading = true;
+    this.hasError = false;
+    this.requestSubscription = this.searchUsersService.getUsers(this.query, this.page).subscribe(({
+      next: (res => {
+        this.totalPages = res.data?.totalPages ?? 0;
+        this.users = res.data?.users;
+        this.loading = res.loading;
+        this.hasError = !!res.error;
+      }),
+      error: (err => {
+        alert("Unpredictable Error");
+        console.error("Failed to load: ", err)
+        // this.hasError = !!err;
+        // this.loading = false;
+        // this.users = [];
+      })
+    }));
+  }
+
+  ngOnDestroy(): void {
+    this.requestSubscription?.unsubscribe()
+  }
+
+  previous() {
+    this.page--;
+    this.doSearch();
+  }
+
+  next() {
+    this.page++;
+    this.doSearch();
+  }
+}
+
+const LIMIT = 10;
+const DELAY = 2000;
+const getUrl = (query: string, page: number): string => {
+  const skip = (page - 1) * LIMIT;
+  return `https://dummyjson.com/users/search?q=${query}&limit=${LIMIT}&skip=${skip}&delay=${DELAY}`;
+}
+
+function convertToUsers(rawData: any): { users: User[], totalPages: number } {
+  const users = (rawData.users as User[]).map(({id, firstName, lastName}) => ({id, firstName, lastName}));
+  const totalPages = Math.ceil(rawData.total / LIMIT);
+  return {users, totalPages}
+}
+
+function debounce<T extends (...args: any[]) => void>(fn: T, delay: number = 500): (...args: Parameters<T>) => void {
+  let timerId: ReturnType<typeof setTimeout>;
+  return function (this: ThisParameterType<T>, ...args: Parameters<T>) {
+    clearTimeout(timerId);
+    timerId = setTimeout(() => {
+      fn.apply(this, args)
+    }, delay);
+  };
+}
